ee38d99efe54c6766d79d830f1efc1daab641f7a
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 21:21:20 +0600

    Initial Commit

---
+++ Tut4_PathAndSHapes/Tut4_PathAndSHapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut4_PathAndSHapesApp.swift
+//  Tut4_PathAndSHapes
+//
+//  Created by ZEUS on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Tut4_PathAndSHapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
