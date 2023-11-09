32ec871988006714212f078579e0aed21cc45281
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 22:12:20 +0600

    CircleLoaderView

--- Tut4_PathAndSHapes/ContentView.swift
+++ Tut4_PathAndSHapes/ContentView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ArcsAndPieCharts()
+           // ArcsAndPieCharts()
            // RectangleView()
+            CircleLoaderView()
+                .padding()
             DomeView()
         }
     }
@@ -98,5 +100,33 @@ struct ArcsAndPieCharts: View {
                     .offset(x:60, y: 60)
             )
         }
+    }
+}
+
+struct CircleLoaderView: View{
+    private var purpleGradient = LinearGradient(gradient: Gradient(colors: [ Color(red: 207/255, green: 150/255, blue: 207/255), Color(red: 107/255, green: 116/255,blue: 179/255) ]), startPoint: .trailing, endPoint: .leading)
+    var body: some View{
+        ZStack{
+            Circle()
+                .stroke(Color(.systemGray6), lineWidth: 20)
+                .frame(width: 300, height: 300)
+            
+            Circle()
+                .trim(from: 0, to: 0.85)
+                .stroke(purpleGradient, lineWidth: 20)
+                .frame(width: 300, height: 300)
+                .overlay(){
+                    VStack{
+                        Text("85%")
+                            .font(.system(size: 60, weight: .semibold, design: .rounded))
+                            .foregroundColor(.gray)
+                        Text("Complete")
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            .foregroundColor(.gray)
+                    }
+                }
+        }
+        
+        
     }
 }
