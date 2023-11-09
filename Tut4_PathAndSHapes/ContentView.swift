d2b057c7bf943d85149f0f413f44c28fd586fed4
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-11-09 22:25:00 +0600

    DonutChartView

--- Tut4_PathAndSHapes/ContentView.swift
+++ Tut4_PathAndSHapes/ContentView.swift
@@ -12,10 +12,13 @@ struct ContentView: View {
         VStack {
            // ArcsAndPieCharts()
            // RectangleView()
+//            DomeView()
+
             CircleLoaderView()
+                .padding(.bottom, 20.0)
+            DonutChartView()
                 .padding()
-            DomeView()
-        }
+        }.padding()
     }
 }
 
@@ -128,5 +131,31 @@ struct CircleLoaderView: View{
         }
         
         
+    }
+}
+
+struct DonutChartView: View{
+    var body: some View{
+        ZStack {
+            Circle()
+                .trim(from: 0, to: 0.3)
+            .stroke(Color.green, lineWidth: 60)
+            Circle()
+                .trim(from: 0.3, to: 0.5)
+                .stroke(Color(.systemTeal), lineWidth: 60)
+            Circle()
+                .trim(from: 0.5, to: 0.7)
+                .stroke(Color(.purple), lineWidth: 60)
+            Circle()
+                .trim(from: 0.7, to: 1)
+                .stroke(Color(.systemYellow), lineWidth: 60)
+                .overlay(){
+                    Text("25%")
+                        .font(.system(size: 20, weight: .bold, design: .monospaced))
+                        .foregroundColor(.white)
+                        .offset(x: 120, y: -110)
+                }
+        }
+
     }
 }
